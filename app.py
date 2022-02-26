aa92e705779c8db66c4a16ae94298a587e1f3962
arkanunn <91520237+arkanunn@users.noreply.github.com>
2022-02-26 12:39:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -151,13 +151,16 @@ def event_handle(event,json_line):
         msg = str(event["message"]["text"])
         if msg == "สวัสดี":
             replyObj = TextSendMessage(text="ดีด้วย")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "กินข้าวไหม":
             replyObj = TextSendMessage(text="ไม่ล่ะ กินแล้ว")
+            line_bot_api.reply_message(rtoken, replyObj)
         elif msg == "ไปเที่ยวกันไหม":
             replyObj = TextSendMessage(text="เยี่ยมเลย")
+            line_bot_api.reply_message(rtoken, replyObj)
         else:
             replyObj = TextSendMessage(text=msg)
-        line_bot_api.reply_message(rtoken, replyObj)    
+            line_bot_api.reply_message(rtoken, replyObj)    
     elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
